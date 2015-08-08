09038567ebd01ca03d06193ff64cb2e846216880
HUIZHANGSAMA <867154252@qq.com>
2015-08-08 18:55:39 +0800

    v8

--- desktop/src/com/rpsg/rpg/desktop/Test.java
+++ desktop/src/com/rpsg/rpg/desktop/Test.java
@@ -1,5 +1,7 @@
 package com.rpsg.rpg.desktop;
 
+import java.lang.reflect.Method;
+
 import com.eclipsesource.v8.V8;
 import com.eclipsesource.v8.V8Object;
 
@@ -10,25 +12,48 @@ public class Test {
 	}
 
 	V8 v8;
+
 	public void start() throws InterruptedException {
-		v8=V8.createV8Runtime();
-		v8.registerJavaMethod(this, "postMessage", "postMessage", new Class<?>[] {String[].class }, true);
+		v8 = V8.createV8Runtime();
+		v8.registerJavaMethod(this, "postMessage", "postMessage",
+				new Class<?>[] { String[].class }, true);
 		v8.registerJavaMethod(this, "tex", "tex", new Class<?>[] {}, false);
-		V8Object obj=v8.executeObjectScript("tex");
-		V8Object tex=obj.getRutime().executeObjectScript("tex.prototype");
-		tex.registerJavaMethod(this, "postMessage", "postMessage", new Class<?>[] {String[].class }, true);
+		V8Object obj = v8.executeObjectScript("tex");
+		V8Object tex = obj.getRutime().executeObjectScript("tex.prototype");
+		tex.registerJavaMethod(this, "postMessage", "postMessage",
+				new Class<?>[] { String[].class }, true);
 		obj.release();
 		tex.release();
-		
+
 		v8.executeVoidScript("var tex=new tex();tex.postMessage('a');postMessage('b')");
 	}
-	
-	public void tex(){
+
+	public void tex() {
 		System.out.println("a new 'tex' javascript object was created.");
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		new Test().start();
+		Test t = new Test();
+		t.registerBridge(new Abc());
+
+		//t.v8.executeVoidScript("var abc = new Abc();abc.a();");
+	}
+
+	public void registerBridge(Object obj) {
+		v8 = V8.createV8Runtime();
+		Class<?> c = obj.getClass();	
+		Method[] methods = c.getDeclaredMethods();
+		for (Method m : methods) {
+
+			v8.registerJavaMethod(obj, m.getName(), m.getName(),
+					m.getParameterTypes(), false);
+		}	
+	
+		V8Object object = v8.executeObjectScript(c.getSimpleName());
+		V8Object o = object.getRutime().executeObjectScript(c.getSimpleName()+".prototype");
+		object.release();
+		o.release();
+
 	}
 
 }
