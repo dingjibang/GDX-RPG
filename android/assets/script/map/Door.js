91091f323c0d0620847497064750ab620c085d52
dingjibang <dingjibang@qq.com>
2015-11-09 16:38:28 +0800

    clear

--- android/assets/script/map/Door.js
+++
@@ -1,8 +0,0 @@
-eval(""+load('global.js'));
-
-if (npc.currentImageNo == NPC.FACE_D) {
-	npc.layer--;
-	npc.currentImageNo = NPC.FACE_U;
-	npc.collideFaceAble = npc.collideFaceZAble = npc.collideFootAble = npc.collideNearAble = npc.collideZAble = true;
-}
-	
