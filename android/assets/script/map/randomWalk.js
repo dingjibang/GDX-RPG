e666aa009a55bdfbf59b148c4a70f3f49a511b31
dingjibang <dingjibang@qq.com>
2016-01-08 11:10:13 +0800

    doc at randomWalk.js

--- android/assets/script/map/randomWalk.js
+++ android/assets/script/map/randomWalk.js
@@ -1,12 +1,42 @@
 eval(""+load('global.js'));
 
+
+/** 
+ *	###随机移动脚本###
+ * 
+ * 
+ * 
+ * 	##使用方法##
+ * 
+ * 	在tiled指定一个NPC后，将其的AUTO_SCRIPT属性设置为此文件的文件名（randomWalk.js）
+ * 
+ * 
+ * 
+ *  ##参数##
+ * 
+ *	#_point : Vector2
+ *	(point这个词为引擎关键字(执行器指针，参见script.java)，被占用的，所以使用_point)
+ *	_point 是一个vector2(vector2 = {x:Number,y:Number}) 变量
+ *	当_point是空值(undefined)的时候，NPC会随机移动
+ *  当_point = {x:-1,y:-1} 时，NPC会朝向玩家缓慢移动（但注意的是，也会偶尔转身往别的地方走，而不是死死的面向玩家）
+ *  否则，当_point的x与y的值为任意数字时，NPC会在这个(x,y)坐标附近移动
+ * 
+ *  #speed : Number
+ * 	行走等待速度，指脚本执行完毕后，下一次重新执行的延时，越高行走越慢，默认60（1秒）
+ *
+ * 	#maxLength : Number
+ * 	 行走最大可能长度，比如每次行走最大走3步，默认为3。
+ * 
+ *  #bounds : Vector2
+ * 	根据x和y，制定一个矩形区域，NPC会在这个矩形内行走而不会越界，越界就会减速并掉头，他的优先级是最高的，并不会屌前面的参数	
+ * 
+ */
+
 npc.walkSpeed=1;
 
-//speed = 行走等待速度，越高行走越慢，默认60（1秒）
 if(typeof(speed)=="undefined" || speed==null)
 	var speed = 60;
 	
-//maxLength = 行走最大可能长度，比如每次最大走3步
 if(typeof(maxLength)=="undefined" || maxLength==null)
 	var maxLength = 3;
 
