ca1b385f376dbefa87692a335690da8ddd45e6c7
dingjibang <dingjibang@qq.com>
2016-02-28 17:32:19 +0800

    status UI

--- core/src/com/rpsg/rpg/system/ui/Status.java
+++ core/src/com/rpsg/rpg/system/ui/Status.java
@@ -3,11 +3,13 @@ package com.rpsg.rpg.system.ui;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.rpsg.gdxQuery.$;
 import com.rpsg.rpg.core.Setting;
@@ -24,25 +26,28 @@ public class Status extends Group {
 	boolean flag;
 	long time = System.currentTimeMillis();
 	private List<DelayTask> tasks = new ArrayList<>();
+	Color shit = new Color(.33f,.16f,.07f,1);
+	Image up = Res.getNP(Setting.IMAGE_BATTLE+"info_u.png"),down = Res.getNP(Setting.IMAGE_BATTLE+"info_d.png");
 	
 	public Status() {
 		table = new Table();
 		table.align(Align.bottom);
-		
 		pane = new ScrollPane(table);
-		pane.setPosition(0, GameUtil.screen_height - 28);
+		pane.setPosition(0, GameUtil.screen_height - 31);
 		pane.setOverscroll(false, false);
-		pane.setHeight(28);
-		pane.setWidth(GameUtil.screen_width - 28);
+		pane.setHeight(31);
+		pane.setWidth(GameUtil.screen_width);
 		pane.setScrollingDisabled(true, false);
-		pane.getStyle().background = Res.getDrawable(Setting.UI_GRAY_IMG);
+		pane.getStyle().background = new NinePatchDrawable(GameUtil.processNinePatchFile(Setting.IMAGE_BATTLE+"infobox.png", 2,2,1,1,5,5,0,3));
 		pane.getStyle().vScroll=Res.getDrawable(Setting.IMAGE_MENU_EQUIP+"mini_scrollbar.png");
 		pane.getStyle().vScrollKnob=Res.getDrawable(Setting.IMAGE_MENU_EQUIP+"mini_scrollbarin.png");
 		pane.setFadeScrollBars(false);
 		
+		$.add(up,down).setPosition(GameUtil.screen_width / 2 - up.getWidth() / 2, GameUtil.screen_height - 37).appendTo(this);
+		
 		addActor(pane);
 		
-		addActor(Res.get(Setting.IMAGE_BATTLE+"status_more.png").position(GameUtil.screen_width - 28, GameUtil.screen_height-28).onClick(()->toggle()));
+		$.add(pane).onClick(()->toggle());
 		
 	}
 	
@@ -58,7 +63,7 @@ public class Status extends Group {
 	
 	public Status add(String str,long delay){
 		tasks.add(new DelayTask(delay,()->{
-			table.add(label = Res.get("["+ getTime() + "] " + str, 20).align(getAlign()).markup(true)).padLeft(10).padRight(10).height(28).width(GameUtil.screen_width - 48).row();
+			table.add(label = Res.get("["+ getTime() + "] " + str, 20).color(shit).align(getAlign()).markup(true)).padLeft(10).padRight(10).height(28).width(GameUtil.screen_width - 48).row();
 			setPanelToScrollBattom();
 		}));
 		return this;
@@ -67,8 +72,11 @@ public class Status extends Group {
 	public void toggle(){
 		flag = !flag;
 		pane.clearActions();
-		pane.addAction(Actions.moveTo(0, GameUtil.screen_height - (flag ? 500 : 28),.4f,Interpolation.pow4Out));
-		pane.addAction(Actions.sequence(Actions.sizeTo(GameUtil.screen_width - 28, flag ? 500 : 28,.4f,Interpolation.pow4Out),Actions.run(()->{
+		pane.addAction(Actions.moveTo(0, GameUtil.screen_height - (flag ? 500 : 31),.4f,Interpolation.pow4Out));
+		up.addAction(Actions.moveTo(GameUtil.screen_width / 2 - up.getWidth() / 2, GameUtil.screen_height - (flag ? 506 : 37),.4f,Interpolation.pow4Out));
+		down.addAction(Actions.moveTo(GameUtil.screen_width / 2 - up.getWidth() / 2, GameUtil.screen_height - (flag ? 506 : 37),.4f,Interpolation.pow4Out));
+		if(flag) up.setZIndex(23333); else down.setZIndex(2333);
+		pane.addAction(Actions.sequence(Actions.sizeTo(GameUtil.screen_width, flag ? 500 : 31,.4f,Interpolation.pow4Out),Actions.run(()->{
 			if(!flag) setPanelToScrollBattom();
 		})));
 		
