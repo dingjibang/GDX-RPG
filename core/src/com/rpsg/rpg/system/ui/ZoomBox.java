f9110029a6eb8c71348f82d4dab6d1767c1d8430
Rgonister <1352485083@qq.com>
2016-07-24 22:00:01 +0800

    .

--- core/src/com/rpsg/rpg/system/ui/ZoomBox.java
+++ core/src/com/rpsg/rpg/system/ui/ZoomBox.java
@@ -12,56 +12,56 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 /**
  * 缩放组件
  */
-public class ZoomBox extends Actor{
-	
+public class ZoomBox extends Actor {
 	private Image img;
-	
-	private static final float rate = 0.01f;
-	
-	//传入要缩放的图片，以及zoombox初始时的宽高
-	public ZoomBox(Image image,int width,int height) {
-		//设置宽高
+
+	private static final float rate = 0.03f;
+
+	// 传入要缩放的图片，以及zoombox初始时的宽高
+	public ZoomBox(Image image, int width, int height) {
+		// 设置宽高
 		this.setWidth(width);
 		this.setHeight(height);
 		img = image;
 		img.setWidth(width);
 		img.setHeight(height);
-		
-		//将图片等比拉抻到当前宽高下最大
-		//TODO
-		
-		//注册输入监听器
-		addListener(new InputListener(){
-			//手或鼠标按下时触发
+
+		// 将图片等比拉抻到当前宽高下最大
+		// TODO
+
+		// 注册输入监听器
+		addListener(new InputListener() {
+			// 手或鼠标按下时触发
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				// TODO Auto-generated method stub
 				return super.touchDown(event, x, y, pointer, button);
 			}
-			
-			//手或鼠标松开时触发
+
+			// 手或鼠标松开时触发
 			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				// TODO Auto-generated method stub
 				super.touchUp(event, x, y, pointer, button);
 			}
-			
-			//手或鼠标拖曳时触发
+
+			// 手或鼠标拖曳时触发
 			public void touchDragged(InputEvent event, float x, float y, int pointer) {
-				// TODO Auto-generated method stub
 				super.touchDragged(event, x, y, pointer);
 			}
-			
-			//鼠标使用滚轮时触发
+
+			// 鼠标使用滚轮时触发
 			public boolean scrolled(InputEvent event, float x, float y, int amount) {
-				float f = (1+amount*rate);
-				img.setWidth(img.getWidth()*f);
-				img.setHeight(img.getHeight()*f);
+				float f = (1 + amount * rate);
+				float x1 = img.getX();
+				float y1 = img.getY();
+				float ox = amount * rate * img.getWidth() / 2;
+				float oy = amount * rate * img.getHeight() / 2;
+				img.setPosition(x1 - ox, y1 - oy);
+				img.setWidth(img.getWidth() * f);
+				img.setHeight(img.getHeight() * f);
 				return super.scrolled(event, x, y, amount);
 			}
-			
+
 		});
-		
-		
-		Gdx.input.setInputProcessor(new GestureDetector(new GestureListener(){
+
+		Gdx.input.setInputProcessor(new GestureDetector(new GestureListener() {
 
 			@Override
 			public boolean touchDown(float x, float y, int pointer, int button) {
@@ -101,9 +101,14 @@ public class ZoomBox extends Actor{
 
 			@Override
 			public boolean zoom(float initialDistance, float distance) {
-				float f = distance/100;
-				img.setWidth(img.getWidth()*f);
-				img.setHeight(img.getHeight()*f);
+				float f = 1 + distance / 100;
+				float x1 = img.getX();
+				float y1 = img.getY();
+				float ox = distance / 100 * rate * img.getWidth() / 2;
+				float oy = distance / 100 * rate * img.getHeight() / 2;
+				img.setPosition(x1 - ox, y1 - oy);
+				img.setWidth(img.getWidth() * f);
+				img.setHeight(img.getHeight() * f);
 				return false;
 			}
 
@@ -111,10 +116,11 @@ public class ZoomBox extends Actor{
 			public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
 				// TODO Auto-generated method stub
 				return false;
-			}}));
+			}
+		}));
 	}
-	
-	public void draw (Batch batch, float parentAlpha) {
-		img.draw(batch, parentAlpha);		
+
+	public void draw(Batch batch, float parentAlpha) {
+		img.draw(batch, parentAlpha);
 	}
 }
