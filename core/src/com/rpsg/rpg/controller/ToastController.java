8da2cc857973105e57eb4d693e28e6abcf6300d5
Lchaos <770844276@qq.com>
2017-11-07 03:20:41 +0900

    ToastView Improved

--- core/src/com/rpsg/rpg/controller/ToastController.java
+++ core/src/com/rpsg/rpg/controller/ToastController.java
@@ -11,52 +11,32 @@ import script.ui.view.ToastView;
  */
 public class ToastController {
 
-	public static final int def_delaytime = 5000;
+	public static final int def_delaytime = 3000;
 	
 	public void showToast(String text,int fontsize)
 	{
 		showToast(text,fontsize,def_delaytime);
 	}
 	
-	public void showToast(String text,int fontsize,int delaytime)
-	{
-		showToast(text,fontsize,delaytime,ToastView.def_bgcolor);
-	}
-	
-	public void showToast(String text,int fontsize,int delaytime,String bgcolor)
-	{
+	public void showToast(String text,int fontsize,int delaytime){
 		ToastView vw = new ToastView();
 		vw.text = text;
 		vw.fontsize = fontsize;
-		vw.bgcolor = bgcolor;
-		//if(Setter!= null) Setter.setter(vw);
 		vw.create();
 		Views.addView(vw);
-		Timer.add(TimeType.millisecond, delaytime, new Runnable(){
-			public void run()
-			{
-				vw.remove();
-			}
-		});
+		vw.setRemoveMessage(delaytime);
 	}
 	
-	public void showToast(String text,ToastViewSetter Setter,int delaytime)
-	{
+	public void showToast(String text,ToastViewSetter Setter,int delaytime){
 		ToastView vw = new ToastView();
 		vw.text = text;
 		if(Setter!= null) Setter.setter(vw);
 		vw.create();
 		Views.addView(vw);
-		Timer.add(TimeType.millisecond, delaytime, new Runnable(){
-			public void run()
-			{
-				vw.remove();
-			}
-		});
+		vw.setRemoveMessage(delaytime);
 	}
 	
-	public static abstract class ToastViewSetter
-	{
+	public static abstract class ToastViewSetter{
 		public abstract void setter(ToastView vw);
 	}
 }
--- core/src/script/ui/view/ToastView.groovy
+++ core/src/script/ui/view/ToastView.groovy
@@ -12,7 +12,10 @@ import com.rpsg.gdxQuery.GdxQuery
 import com.rpsg.gdxQuery.TypedGdxQuery
 import com.rpsg.rpg.core.*
 import com.rpsg.rpg.ui.widget.Button
+import com.rpsg.rpg.ui.widget.Image
 import com.rpsg.rpg.util.Position
+import com.rpsg.rpg.util.Timer.TimeType
+import com.rpsg.rpg.util.Timer
 
 import script.ui.widget.menu.HeroFGLabel
 import script.ui.widget.menu.HeroSelector
@@ -35,6 +38,7 @@ class ToastView extends UIView{
 	public static final float def_bottommagin = 36.0;
 	public static final String def_bgcolor = "333333cc";
 	public static final int def_fontsize = 26;
+	public static final float def_fadetime = 0.5f;
 	
 	public float interlvalheight = def_interlvalheight;
 	public float interlvalwidth = def_interlvalwidth;
@@ -46,32 +50,62 @@ class ToastView extends UIView{
 	static{
 		UI_TOAST = new NinePatchDrawable(UI.get9(Path.IMAGE_GLOBAL + "toast_box.png",2,2,1,1,5,5,5,5));
 	}
-	
+	GdxQuery bg
+	GdxQuery grp 
 	void create() {
-		def grp = q(new Group()) to stage //
+		bg = q(new Image( UI_TOAST)) to stage
+		grp = q(new Group()) to stage //
 		//table.addActor(UI.base().query().size(870	, 180).color(Color.BLACK).a(0.3f).y(-17).get())
 		//def table = new Table().top().left()
 		
 		//grp.position(360, 36)
 		//grp.width(Game.STAGE_WIDTH)
 		//grp.center()
+		
 		String[] lines = text.split("\n");
 	    float curbottommagin = bottommagin;
-		for(int i=lines.length;i>=0;i--){
-			def label = $(text, fontsize) to grp
+		float totalh = 0;
+		float maxwidth = 0;
+		for(int i=lines.length -1;i>=0;i--){
+			def label = $(lines[i], fontsize) to grp
 			def w = label.width()
 			label.position(Math.max((float)(Game.STAGE_WIDTH/2f - w/2f),0f),curbottommagin)
-			curbottommagin += label.length();
+			curbottommagin += label.height();
+			
+			//label.action( Actions.fadeIn(1f))
+			if(maxwidth<w) maxwidth =w;
 		}
 		
-		def bg = q("base").size(Math.min((float)(label.width() + interlvalwidth),(float)Game.STAGE_WIDTH),
-			Math.min((float)( label.height() + interlvalheight),(float)Game.STAGE_HEIGHT)).
+		totalh = curbottommagin - bottommagin;
+		
+		
+		bg.size(Math.min((float)(maxwidth + interlvalwidth),(float)Game.STAGE_WIDTH),
+			Math.min((float)(totalh + interlvalheight),(float)Game.STAGE_HEIGHT)).
 		color(bgcolor).
-		x(Math.max((float)(Game.STAGE_WIDTH/2 - w/2f - interlvalwidth/2f), 0f) ).
-		y(Math.max((float)(bottommagin - interlvalheight/2f),0f)) to stage
+		x(Math.max((float)(Game.STAGE_WIDTH/2f - maxwidth/2f - interlvalwidth/2f), 0f)).
+		y(Math.max((float)(bottommagin - interlvalheight/2f),0f)) 
+
+		bg.a(0f).action( Actions.fadeIn(def_fadetime))
+		grp.a(0f).action(Actions.fadeIn(def_fadetime))
+		this.buuleable(true)
 		//label.center(0)
 		//table to grp 
 	}
 	
+	void setRemoveMessage(int delayTime){
 
+		def vw = this;
+		Timer.add(TimeType.millisecond, delayTime, new Runnable(){
+			public void run()
+			{
+				bg.action( Actions.fadeOut(def_fadetime))
+				grp.action(Actions.fadeOut(def_fadetime))
+			}
+		})
+		Timer.add(TimeType.millisecond, (int)(def_fadetime * 1000) + delayTime, new Runnable(){
+			void run(){
+				vw.remove();
+			}
+		})
+	}
 }
